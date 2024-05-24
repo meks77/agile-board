42652aa37575f0d1e59e4d85885db5a08e74c64b
Markus Hager <meks77@gmail.com>
2024-05-24 17:35:36 +0200

    Space Build Job

--- .space.kts
+++ .space.kts
@@ -4,16 +4,6 @@
  * For more info, see https://www.jetbrains.com/help/space/automation.html
  */
 
-job("Build backend") {
-    container(displayName = "Run gradlew from another dir", image = "amazoncorretto:21-alpine") {
-        workDir = "backend"
-
-        kotlinScript { api ->
-            api.gradlew("build")
-        }
-    }
-}
-
 job("Test backend") {
     container(displayName = "Run gradlew from another dir", image = "amazoncorretto:21-alpine") {
         workDir = "backend"
